42fed66de53f79088616731a76fbeb18d932877b
hasiniWijerathna <hasini.2014198@iit.ac.lk>
2017-06-28 15:28:04 +0530

    Added sign in promises

--- src/actions/SessionActions.js
+++ src/actions/SessionActions.js
@@ -1,2 +1,52 @@
 import {authenticate} from '../services/SessionService';
 import {setSession, getSession} from '../store';
+
+export const SIGN_IN_REQUEST = SESSION::SIGN_IN;
+
+export const SIGN_IN_SUCCESS = SESSION::SIGN_IN::SUCCESS;
+
+export const SIGN_IN_FAIL = SESSION::SIGN_IN::FAIL
+
+export const SIGN_OUT = SESSION::SIGN_OUT;
+
+const userSignInSuccessful (response) => {
+
+  const {user, token} = response;
+
+  return {
+    type: SIGN_IN_SUCCESS,
+
+    payload: {
+      token,
+      user,
+    },
+  },
+}
+
+const userSignInFailed (error) => {
+
+  return {
+    type: SIGN_IN_FAIL,
+
+    payload : {
+      error.
+    },
+  },
+}
+
+const signInUser (credentials) => {
+  const {email, password} = credentials;
+
+  return (dispatch) => {
+      dispatch ({type : SIGN_IN_REQUEST });
+
+      authenticate(email, password)
+      .then ((response) => {
+       setSession(response);
+
+       dispatch(userSignInSuccessful(getSession()));
+      })
+      .catch((error) => dispatch(userSignInFailed(error.message)));
+  }
+
+}
